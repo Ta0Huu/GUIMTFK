d5dc16cbc24bfb17e2c9cdc99ffb0a493b996071
Your Name <you@example.com>
2023-03-03 15:17:08 +0700

    saved

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
 
+using namespace std;
+
+int main(int argc,char* argv[]){
+    float ave=0;
+    if(argc <= 1)cout << "Please input numbers to find average.\n";
+    else{
+    
+    for(int i = 0; i < argc;i++){
+        ave += atof(argv[i]);}
+    cout << "---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = "<< ave/(argc-1) <<endl;
+    cout << "---------------------------------\n";
+    }
+
+
+}
